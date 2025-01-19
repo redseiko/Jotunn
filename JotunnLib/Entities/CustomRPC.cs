4ad9255de44652ef389b872630856f68ebf28755
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-01-19 10:40:42 +0100

    chore: cleanup CustomRPC

--- JotunnLib/Entities/CustomRPC.cs
+++ JotunnLib/Entities/CustomRPC.cs
@@ -22,36 +22,45 @@ namespace Jotunn.Entities
         private const byte FRAGMENTED_PACKAGE = 2;
         private const byte COMPRESSED_PACKAGE = 4;
 
+        private const int CompressMinSize = 10000;
+        private const int PackageSliceSize = 250000;
+        private const int MaximumSendQueueSize = 20000;
+
+        private const float Timeout = 30f;
+
         /// <summary>
         ///     Name of the custom RPC as defined at instantiation
         /// </summary>
         public string Name { get; }
-        
+
         /// <summary>
         ///     True, if this RPC is currently sending data
         /// </summary>
         public bool IsSending => SendCount > 0;
+
         /// <summary>
         ///     True, if this RPC is currently receiving data
         /// </summary>
         public bool IsReceiving => PackageCache.Count > 0;
+
         /// <summary>
         ///     True, if this RPC is currently processing received data.
         ///     This is always true while executing the registered delegates.
         /// </summary>
         public bool IsProcessing => ProcessingCount > 0;
+
         /// <summary>
         ///     True, if this RPC is processing received data outside the current delegate call.
         ///     This should only be used in the registered delegate methods to determine
         ///     if this RPC is already processing another package.
         /// </summary>
-        public bool IsProcessingOther => ProcessingCount-1 > 0;
+        public bool IsProcessingOther => ProcessingCount - 1 > 0;
 
         /// <summary>
         ///     Unique ID of this RPC to prevent name clashes between mods
         /// </summary>
         internal string ID => $"{SourceMod.GUID}!{Name}";
-        
+
         /// <summary>
         ///     Delegate called when a package is received on the server
         /// </summary>
@@ -65,10 +74,9 @@ namespace Jotunn.Entities
         private short SendCount;
         private short ProcessingCount;
         private long PackageCount;
-        private readonly Dictionary<string, SortedDictionary<int, byte[]>> PackageCache =
-            new Dictionary<string, SortedDictionary<int, byte[]>>();
-        private readonly List<KeyValuePair<long, string>> CacheExpirations =
-            new List<KeyValuePair<long, string>>(); // avoid leaking memory
+
+        private readonly Dictionary<string, SortedDictionary<int, byte[]>> PackageCache = new Dictionary<string, SortedDictionary<int, byte[]>>();
+        private readonly List<KeyValuePair<long, string>> CacheExpirations = new List<KeyValuePair<long, string>>(); // avoid leaking memory
 
         /// <summary>
         ///     Internal constructor only, CustomRPCs are instantiated via <see cref="NetworkManager"/>
@@ -77,8 +85,7 @@ namespace Jotunn.Entities
         /// <param name="name"></param>
         /// <param name="serverReceive"></param>
         /// <param name="clientReceive"></param>
-        internal CustomRPC(BepInPlugin sourceMod, string name, NetworkManager.CoroutineHandler serverReceive,
-            NetworkManager.CoroutineHandler clientReceive) : base(sourceMod)
+        internal CustomRPC(BepInPlugin sourceMod, string name, NetworkManager.CoroutineHandler serverReceive, NetworkManager.CoroutineHandler clientReceive) : base(sourceMod)
         {
             Name = name;
             OnServerReceive = serverReceive;
@@ -86,28 +93,32 @@ namespace Jotunn.Entities
         }
 
         /// <summary>
-        ///     Initiates a RPC exchange with the server by sending an empty package.
+        ///     Initiates an RPC exchange with the server by sending an empty package.
         /// </summary>
-        public void Initiate() =>
-            ZNet.instance?.StartCoroutine(SendPackageRoutine(
-                ZRoutedRpc.instance.GetServerPeerID(),
-                new ZPackage(new[] { JOTUNN_PACKAGE })));
-        
+        public void Initiate()
+        {
+            ZNet.instance?.StartCoroutine(SendPackageRoutine(ZRoutedRpc.instance.GetServerPeerID(), new ZPackage(new[] { JOTUNN_PACKAGE })));
+        }
+
         /// <summary>
         ///     Send a package to a single target. Compresses and fragments the package if necessary.
         /// </summary>
         /// <param name="target"></param>
         /// <param name="package"></param>
-        public void SendPackage(long target, ZPackage package) =>
+        public void SendPackage(long target, ZPackage package)
+        {
             ZNet.instance?.StartCoroutine(SendPackageRoutine(target, package));
+        }
 
         /// <summary>
         ///     Send a package to a list of peers. Compresses and fragments the package if necessary.
         /// </summary>
         /// <param name="peers"></param>
         /// <param name="package"></param>
-        public void SendPackage(List<ZNetPeer> peers, ZPackage package) =>
+        public void SendPackage(List<ZNetPeer> peers, ZPackage package)
+        {
             ZNet.instance?.StartCoroutine(SendPackageRoutine(peers, package));
+        }
 
         /// <summary>
         ///     Coroutine to send a package to a single target. Compresses and fragments the package if necessary.
@@ -121,16 +132,16 @@ namespace Jotunn.Entities
             {
                 return Enumerable.Empty<object>().GetEnumerator();
             }
-            
+
             List<ZNetPeer> peers = ZRoutedRpc.instance.m_peers;
+
             if (target == ZRoutedRpc.instance.m_id || target == ZRoutedRpc.Everybody)
             {
                 byte[] originalData = package.GetArray();
                 ZPackage jotunnpackage = new ZPackage();
                 jotunnpackage.Write(originalData);
                 jotunnpackage.SetPos(0);
-                ZNet.instance.StartCoroutine(
-                    HandlePackageRoutine(ZRoutedRpc.instance.m_id, jotunnpackage, JOTUNN_PACKAGE));
+                ZNet.instance.StartCoroutine(HandlePackageRoutine(ZRoutedRpc.instance.m_id, jotunnpackage, JOTUNN_PACKAGE));
             }
             else
             {
@@ -148,16 +159,11 @@ namespace Jotunn.Entities
         /// <returns></returns>
         public IEnumerator SendPackageRoutine(List<ZNetPeer> peers, ZPackage package)
         {
-            if (!ZNet.instance)
+            if (!ZNet.instance || peers.Count == 0)
             {
                 yield break;
             }
 
-            if (peers.Count == 0)
-            {
-                yield break;
-            }
-            
             try
             {
                 ++SendCount;
@@ -168,28 +174,14 @@ namespace Jotunn.Entities
                 jotunnpackage.Write(originalData);
                 package = jotunnpackage;
 
-                const int compressMinSize = 10000;
-
-                if (package.Size() > compressMinSize)
+                if (package.Size() > CompressMinSize)
                 {
-                    byte[] rawData = package.GetArray();
-                    Logger.LogDebug($"[{ID}] Compressing package with length {rawData.Length}");
-
-                    ZPackage compressedPackage = new ZPackage();
-                    compressedPackage.Write(COMPRESSED_PACKAGE);
-                    MemoryStream output = new MemoryStream();
-                    using (DeflateStream deflateStream = new DeflateStream(output, CompressionLevel.Optimal))
-                    {
-                        deflateStream.Write(rawData, 0, rawData.Length);
-                    }
-
-                    compressedPackage.Write(output.ToArray());
-                    package = compressedPackage;
+                    package = CompressPackage(package);
                 }
 
-                List<IEnumerator<bool>> writers =
-                    peers.Where(peer => peer.IsReady()).Select(p => SendToPeer(p, package)).ToList();
+                List<IEnumerator<bool>> writers = peers.Where(peer => peer.IsReady()).Select(p => SendToPeer(p, package)).ToList();
                 writers.RemoveAll(writer => !writer.MoveNext());
+
                 while (writers.Count > 0)
                 {
                     yield return null;
@@ -202,6 +194,24 @@ namespace Jotunn.Entities
             }
         }
 
+        private ZPackage CompressPackage(ZPackage package)
+        {
+            byte[] rawData = package.GetArray();
+            Logger.LogDebug($"[{ID}] Compressing package with length {rawData.Length}");
+
+            ZPackage compressedPackage = new ZPackage();
+            compressedPackage.Write(COMPRESSED_PACKAGE);
+            MemoryStream output = new MemoryStream();
+
+            using (DeflateStream deflateStream = new DeflateStream(output, CompressionLevel.Optimal))
+            {
+                deflateStream.Write(rawData, 0, rawData.Length);
+            }
+
+            compressedPackage.Write(output.ToArray());
+            return compressedPackage;
+        }
+
         /// <summary>
         ///     Coroutine to send a package to an actual peer.
         /// </summary>
@@ -211,22 +221,20 @@ namespace Jotunn.Entities
         private IEnumerator<bool> SendToPeer(ZNetPeer peer, ZPackage package)
         {
             ZRoutedRpc rpc = ZRoutedRpc.instance;
+
             if (rpc == null)
             {
                 yield break;
             }
 
-            const int packageSliceSize = 250000;
-            const int maximumSendQueueSize = 20000;
-
             IEnumerable<bool> WaitForQueue()
             {
-                float timeout = Time.time + 30;
-                while (peer.m_socket.GetSendQueueSize() > maximumSendQueueSize)
+                float timeout = Time.time + Timeout;
+                while (peer.m_socket.GetSendQueueSize() > MaximumSendQueueSize)
                 {
                     if (Time.time > timeout)
                     {
-                        Logger.LogInfo($"Disconnecting {peer.m_uid} after 30 seconds sending timeout");
+                        Logger.LogInfo($"Disconnecting {peer.m_uid} after {Timeout} seconds sending timeout");
                         peer.m_rpc.Invoke("Error", ZNet.ConnectionStatus.ErrorConnectFailed);
                         ZNet.instance.Disconnect(peer);
                         yield break;
@@ -241,11 +249,12 @@ namespace Jotunn.Entities
                 rpc.InvokeRoutedRPC(peer.m_uid, ID, pkg);
             }
 
-            if (package.Size() > packageSliceSize)
+            if (package.Size() > PackageSliceSize)
             {
                 byte[] data = package.GetArray();
-                int fragments = (int)(1 + (data.LongLength - 1) / packageSliceSize);
+                int fragments = (int)(1 + (data.LongLength - 1) / PackageSliceSize);
                 long packageIdentifier = ++PackageCount;
+
                 for (int fragment = 0; fragment < fragments; fragment++)
                 {
                     foreach (bool wait in WaitForQueue())
@@ -263,7 +272,7 @@ namespace Jotunn.Entities
                     fragmentedPackage.Write(packageIdentifier);
                     fragmentedPackage.Write(fragment);
                     fragmentedPackage.Write(fragments);
-                    fragmentedPackage.Write(data.Skip(packageSliceSize * fragment).Take(packageSliceSize).ToArray());
+                    fragmentedPackage.Write(data.Skip(PackageSliceSize * fragment).Take(PackageSliceSize).ToArray());
 
                     Logger.LogDebug($"[{ID}] Sending fragmented package {packageIdentifier}:{fragment}");
                     Send(fragmentedPackage);
@@ -372,7 +381,7 @@ namespace Jotunn.Entities
 
                     Logger.LogDebug($"[{ID}] Decompressed package to length {output.Length}");
                 }
-                
+
                 if ((packageFlags & JOTUNN_PACKAGE) != JOTUNN_PACKAGE)
                 {
                     Logger.LogWarning($"[{ID}] Package flag does not equal {JOTUNN_PACKAGE} ({packageFlags:X4})");
